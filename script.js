08ec2d4a72808a6bb4dbbec861cba7fc106b4a2f
Dharshini-8 <vtdharshini8@gmail.com>
2026-05-12 21:30:51 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document
+.getElementById("loginForm")
+
+.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    let username =
+    document.getElementById("username").value;
+
+    let password =
+    document.getElementById("password").value;
+
+    let message =
+    document.getElementById("message");
+
+    if(username === "admin" && password === "1234"){
+
+        message.style.color = "green";
+
+        message.innerText =
+        "Login Successful!";
+    }
+
+    else{
+
+        message.style.color = "red";
+
+        message.innerText =
+        "Invalid Username or Password";
+    }
+
+});
